0c139cc1d8618930966cf8c002a29e235c9cc75c
Mirek Kratochvil <exa.exa@gmail.com>
2021-01-15 20:00:20 +0100

    finally make the scales to scale
    (This requires a modified ggplot, because scale='none' is just not there. Ask
    me if required.)
    
    Also these are officially called kebab plots from now on.

--- plots.R
+++ plots.R
@@ -15,10 +15,10 @@ d1$taskType <- unlist(lapply(strsplit(as.character(d1$task), split=' '), functio
 d1$taskType <- factor(d1$taskType, levels=c('Textual','Visual'), labels=c("Textual tasks", "Visual tasks"))
 
 d2 <- d[,c('system','task','team','video_rank')]
-d2 <- d2[d2$video_rank>0,]# & d2$video_rank<10000,]
+d2 <- d2[d2$video_rank>0,]# & d2$rank<10000,]
 d2 <- reshape2::melt(reshape2::acast(d2, system~task~team, fun.aggregate=min))
-colnames(d2) <- c('system', 'task', 'team', 'video_rank')
-d2 <- d2[is.finite(d2$video_rank),]
+colnames(d2) <- c('system', 'task', 'team', 'rank')
+d2 <- d2[is.finite(d2$rank),]
 d2$taskType <- unlist(lapply(strsplit(as.character(d2$task), split=' '), function(x)x[2]))
 d2$taskType <- factor(d2$taskType, levels=c('Textual','Visual'), labels=c("Textual tasks", "Visual tasks"))
 
@@ -27,8 +27,8 @@ library(ggplot2)
 #frame ranks
 ggsave("plot-ranks.pdf", width=4.5, height=2, scale=1.5, units='in', 
   ggplot() +
-  geom_violin(data=d1, trim=T, aes(x=system, y=rank, fill=system), size=0, color=rgb(0,0,0,0), adjust=0.3, scale='width') +
-  geom_violin(data=d2, trim=T, aes(x=system, y=video_rank, color=system), fill=rgb(0,0,0,0),size=.8, adjust=0.3, scale='width') +
+  geom_violin(data=d1, trim=T, aes(x=system, y=rank, fill=system), size=0, color=rgb(0,0,0,0), adjust=0.3, scale='none', scale.factor=.59) +
+  geom_violin(data=d2, trim=T, aes(x=system, y=rank, color=system), fill=rgb(0,0,0,0),size=.8, adjust=0.3, scale='none', scale.factor=.59) +
   facet_grid(.~taskType) +
   scale_fill_manual('Best frame rank', values=c('#ffb0a0','#a0b0ff')) +
   scale_color_manual('Best video rank', values=c('#c02000','#0020c0')) +
@@ -36,6 +36,12 @@ ggsave("plot-ranks.pdf", width=4.5, height=2, scale=1.5, units='in',
   scale_y_log10('Rank', breaks=c(1,10,100,1000,10000), labels=function(x)sitools::f2si(x)) +
   theme_minimal()
 )
+
+xs <- seq(0,4.6, length.out=1000)
+dd <- data.frame()
+for(tt in 
+
+
 
 #time
 d <- read.csv('submissions.csv')
